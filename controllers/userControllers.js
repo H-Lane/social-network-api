c3a65eb20c921506d4bb5650007b542e1ab96110
Hersey <helane1996@gmail.com>
2024-07-15 23:42:24 -0400

    began routing

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,2 +1,16 @@
-const {ObjectId} = require(`mongoose`).Types;
-const {User, Thought} = require(`../models`);
+const { ObjectId } = require(`mongoose`).Types;
+const { User, Thought } = require(`../models`);
+
+module.exports = {
+  //GET all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+
+      res.json(users);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+};
